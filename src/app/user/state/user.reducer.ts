4b5489f9f3f50adbdd3282871a493c97069a8d9c
Steven Rogers <steven.rogers1990@gmail.com>
2019-07-29 23:13:43 +0100

    Add user selector

--- src/app/user/state/user.reducer.ts
+++ src/app/user/state/user.reducer.ts
@@ -1,5 +1,6 @@
 import * as fromRoot from '../../state/app.state';
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { User } from '../user';
 
 export interface State extends fromRoot.State {
   users: UserState;
@@ -7,10 +8,12 @@ export interface State extends fromRoot.State {
 
 export interface UserState {
   maskUserName: boolean;
+  currentUser: User;
 }
 
 const initialState: UserState = {
- maskUserName: false
+ maskUserName: false,
+ currentUser: null
 };
 
 const getUserFeatureState = createFeatureSelector<UserState>('users');
@@ -18,6 +21,11 @@ const getUserFeatureState = createFeatureSelector<UserState>('users');
 export const getMaskUserName = createSelector(
   getUserFeatureState,
   state => state.maskUserName
+);
+
+export const getCurrentUser = createSelector(
+  getUserFeatureState,
+  state => state.currentUser
 );
 
 export function reducer(state = initialState, action): UserState {
